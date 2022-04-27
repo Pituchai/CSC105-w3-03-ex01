b9fa6fd63ae483ba83038ef88adb4512795d61e4
Pituchai <Phantom_14706@hotmail.com>
2022-04-27 15:27:21 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; <
+script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+integrity = "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+crossorigin = "anonymous" > < /script>
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Navbar darkcolor = "primary" >
+        <
+        div className = 'container' >
+        <
+        nav herf = "/" > TEST < /nav> <
+        /div> <
+        /Navbar> <
+        /div>
+    );
 }
 
 export default App;
